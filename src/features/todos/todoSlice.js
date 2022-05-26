413e5d46b2c97ac9bdf7963579695b961c7283f1
humphreyokoth <humpasp@gmail.com>
2022-05-26 16:38:36 +0300

    todos reducer

---
+++ src/features/todos/todoSlice.js
@@ -0,0 +1,54 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+
+// Arrays of todoss, loading and error
+const initialState = {
+
+    todos:[],
+    isLoading:false,
+    isError:false,
+    errorMessage:""
+}
+const fetchTodosAPI =async ()=>{
+    const res = await fetch('https://dummyjson.com/todos')
+    const {todos} = res.json()
+    return todos
+}
+
+export const todoSlice = createSlice({
+    name:"todos",
+    initialState,
+    reducers:{},
+    //Three states, pending, fulfilled , rejected
+    extraReducers: builder => { 
+        builder.addCase(fetchTodos.pending,state =>{
+            state.isLoading = true
+        })
+        builder.addCase(fetchTodos.fulfilled,(state,action)=>{
+            state.isLoading = false
+            state.todos =action.payload
+        })
+        builder.addCase(fetchTodos.rejected,(state,action)=>{
+            state.isLoading = false
+            state.isError = true
+            state.errorMessage =action.payload
+        })
+
+    }
+});
+
+export const fetchTodos = createAsyncThunk("todos/fetchTodos",
+    async (thunkAPI)=> {
+    try {
+        const todos = await fetchTodosAPI();
+        return todos;
+    } catch (error) {
+        return thunkAPI.rejectedWithValue(error)
+    }
+        
+    }
+)
+
+export const todosSelector = (state)=> state.todos;
+
+export default todoSlice.reducer;
