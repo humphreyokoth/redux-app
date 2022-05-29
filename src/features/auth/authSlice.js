e6f31682521a3c7585affd1c456ad5f1e084d404
humphreyokoth <humpasp@gmail.com>
2022-05-29 07:23:51 +0300

    authlogout

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -6,10 +6,27 @@ const initialState = {
 isAuthenticated:!!user,
 user:user ? user :null,
 isLoading:false,
-errorMessage:""
+errorMessage:"",
 
 }
 
+const authLogin = async (userData)=> {
+    const res = await fetch ("http://localhost:5000/api/v1/auth/login",{
+    method:"POST",
+    headers:{
+        "COntent-Type":"application/json",
+    },
+    bpdy:JSON.stringify(userData),
+    });
+const resData = await res.json();
+if(!resData.access_token){
+throw new Error(resData.message);
+}
+localStorage.setItem("user",JSON.stringify(resData.uset));
+localStorage.setItem("access_token",resData.access_token);
+localStorage.setItem("refresh_token",resData.refresh_token);
+return resData;
+}
 export const authSlice = createSlice({
     
 })
