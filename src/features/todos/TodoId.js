1bebcfc5bab2129d660b8ee8b1c680f68a5adc27
humphreyokoth <humpasp@gmail.com>
2022-05-30 16:27:11 +0300

    use params

--- src/features/todos/TodoId.js
+++ src/features/todos/TodoId.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React,{useEffect}from 'react'
+import {useSelector,useDispatch} from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchTodo,todosSelector } from './todoSlice';
+ 
 
- const TodoId = () => {
+
+
+const TodoId = () => {
+  const dispatch = useDispatch()
+  const {todo,isLoading,isError,errorMessage}= useSelector(todosSelector)
+  const {id} = useParams()
+
+  useEffect(()=>{
+    dispatch(fetchTodo(id))
+  },[dispatch,id])
   return (
     <div>TodoId</div>
   )
