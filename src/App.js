66f45e7ac0216a5532843a3efce7fd9a357e6cf1
humphreyokoth <humpasp@gmail.com>
2022-06-01 05:28:51 +0300

    app js routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-
-import Todo from './features/todos/Todo';
 import './App.css';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import Todo from './features/todos/Todo';
+import TodoId from './features/todos/TodoId';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-         <Todo/>
+         
+           <Router>
+             <Routes>
+              <Route path='/' element={<Todo/>}/>
+              <Route path='/:id' element ={<TodoId/>}/>
+             </Routes>
+           </Router>
+    
       </header>
     
     </div>
--- src/features/todos/TodoId.js
+++ src/features/todos/TodoId.js
@@ -14,8 +14,21 @@ const TodoId = () => {
   useEffect(()=>{
     dispatch(fetchTodo(id))
   },[dispatch,id])
+
+  if(isLoading){
+    return <div>Loading.....</div>
+  }
+  if(isError){
+    return <div>{errorMessage}</div>
+  }
   return (
+    <div>
+
     <div>TodoId</div>
+    <div>{todo.todo}</div>
+    <h5>Completed:{todo.completed}</h5>
+    <small>{todo.userId}</small>
+    </div>
   )
 }
 
